3979e85d3f5440dba7789b128a97692d440afcc8
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-04-25 19:44:06 +0200

    Define Templates gathering code.

--- utilities/sIBL_GUI_gatherFiles.sh
+++ utilities/sIBL_GUI_gatherFiles.sh
@@ -12,6 +12,24 @@ cp ./utilities/nsis/sIBL_GUI_Setup.exe ./releases/repository/sIBL_GUI.exe
 #! MacOsX Gathering.
 cp ./releases/Darwin/sIBL_GUI.dmg ./releases/repository/sIBL_GUI.dmg
 
+#! Templates Gathering.
+mkdir ./releases/repository/templates/
+cd ./src/templates/3dsMax/
+zip -r ../../../releases/repository/templates/3dsMax_MR_Standard.zip 3dsMax_MR_Standard
+zip -r ../../../releases/repository/templates/3dsMax_Scanline_Standard.zip 3dsMax_Scanline_Standard
+zip -r ../../../releases/repository/templates/3dsMax_VRay_Standard.zip 3dsMax_VRay_Standard
+cd ../Maya
+zip -r ../../../releases/repository/templates/Maya_MR_Standard.zip Maya_MR_Standard
+zip -r ../../../releases/repository/templates/Maya_Rfm_Standard.zip Maya_Rfm_Standard
+zip -r ../../../releases/repository/templates/Maya_Turtle_Standard.zip Maya_Turtle_Standard
+zip -r ../../../releases/repository/templates/Maya_VRay_Dome_Light.zip Maya_VRay_Dome_Light
+zip -r ../../../releases/repository/templates/Maya_VRay_Standard.zip Maya_VRay_Standard
+cd ../XSI
+zip -r ../../../releases/repository/templates/XSI_MR_Standard.zip XSI_MR_Standard
+
+#! Reaching Original Directory.
+cd ../../../
+
 #! sIBL_GUI Change Log Gathering.
 mkdir ./releases/repository/sIBL_GUI\ Change\ Log
 cp -rf ./releases/Change\ Log.html ./releases/repository/sIBL_GUI\ Change\ Log
