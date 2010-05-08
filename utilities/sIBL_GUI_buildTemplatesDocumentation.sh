0ac7495409d3aa0075265f3f1d2482052a5e66de
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-05-08 11:19:33 +0200

    Update various templates headers.

--- utilities/sIBL_GUI_buildTemplatesDocumentation.sh
+++ utilities/sIBL_GUI_buildTemplatesDocumentation.sh
@@ -7,3 +7,6 @@ alias python=/Library/Frameworks/Python.framework/Versions/2.6/bin/python
 
 #! XSI_MR_Standard Template Documentation Building.
 python sIBL_GUI_textileToHtml.py "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/XSI/XSI_MR_Standard/help/XSI_MR_Standard Template Manual" "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/XSI/XSI_MR_Standard/help/XSI_MR_Standard Template Manual.html"  "XSI MR Standard - Template - Manual - Help File"
+
+#! Softimage_MR_Standard Template Documentation Building.
+python sIBL_GUI_textileToHtml.py "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/Softimage/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual" "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/Softimage/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual.html"  "Softimage MR Standard - Template - Manual - Help File"
