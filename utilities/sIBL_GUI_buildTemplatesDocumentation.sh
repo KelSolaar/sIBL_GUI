48805e581588640efb130fb3da0f7e0e2e0cace6
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-05-08 12:01:23 +0200

    Add "Maya_VRay_Dome_Light" template textile documentation.

--- utilities/sIBL_GUI_buildTemplatesDocumentation.sh
+++ utilities/sIBL_GUI_buildTemplatesDocumentation.sh
@@ -19,3 +19,6 @@ python sIBL_GUI_textileToHtml.py "/Users/KelSolaar/Documents/Developement/sIBL_G
 
 #! Maya_Turtle_Standard Template Documentation Building.
 python sIBL_GUI_textileToHtml.py "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/Maya/Maya_Turtle_Standard/help/Maya_Turtle_Standard Template Manual" "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/Maya/Maya_Turtle_Standard/help/Maya_Turtle_Standard Template Manual.html"  "Maya Turtle Standard - Template - Manual - Help File"
+
+#! Maya_VRay_Dome_Light Template Documentation Building.
+python sIBL_GUI_textileToHtml.py "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/Maya/Maya_VRay_Dome_Light/help/Maya_VRay_Dome_Light Template Manual" "/Users/KelSolaar/Documents/Developement/sIBL_GUI/src/templates/Maya/Maya_VRay_Dome_Light/help/Maya_VRay_Dome_Light Template Manual.html"  "Maya VRay Dome Light - Template - Manual - Help File"
