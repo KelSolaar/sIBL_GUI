9be2da8f023c3e69a19346863661165bf12e9062
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-05-19 00:33:05 +0200

    Add "sIBL_GUI_linuxBuild" Linux Os build file.

---
+++ utilities/sIBL_GUI_linuxBuild.sh
@@ -0,0 +1,106 @@
+#/usr/bin/bash
+echo ----------------------------------------------------------------
+echo sIBL_GUI - Linux - Overall Build
+echo ----------------------------------------------------------------
+
+#! Linux Build.
+echo ----------------------------------------------------------------
+echo Build - Begin
+echo ----------------------------------------------------------------
+rm -rf ../releases/Linux/build/
+rm -rf ../releases/Linux/dist/
+python /home/kelsolaar/Softwares/pyinstaller/Makespec.py --noconsole  ../src/sIBL_GUI.py -o ../releases/Linux
+python /home/kelsolaar/Softwares/pyinstaller/Build.py ../releases/Linux/sIBL_GUI.spec
+echo ----------------------------------------------------------------
+echo Build - End
+echo ----------------------------------------------------------------
+
+#! Linux Release.
+echo ----------------------------------------------------------------
+echo Release - Begin
+echo ----------------------------------------------------------------
+rm -rf ../releases/Linux/sIBL_GUI/
+cp -r ../releases/Linux/dist/sIBL_GUI ../releases/Linux/
+mkdir ../releases/Linux/sIBL_GUI/ui/
+cp ../src/ui/sIBL_GUI.ui ../releases/Linux/sIBL_GUI/ui/
+cp ../src/ui/sIBL_GUI_Layouts.rc ../releases/Linux/sIBL_GUI/ui/
+cp ../src/ui/Windows_styleSheet.qss ../releases/Linux/sIBL_GUI/ui/
+cp ../src/ui/Darwin_styleSheet.qss ../releases/Linux/sIBL_GUI/ui/
+cp ../src/ui/Linux_styleSheet.qss ../releases/Linux/sIBL_GUI/ui/
+mkdir ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Icon_Light_48.ico ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/sIBL_GUI_SpashScreen.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/sIBL_GUI_Logo.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Central_Widget.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Central_Widget_Hover.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Central_Widget_Active.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Layout.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Layout_Hover.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Layout_Active.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Miscellaneous.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Miscellaneous_Hover.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Miscellaneous_Active.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Library.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Library_Hover.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Library_Active.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Export.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Export_Hover.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Export_Active.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Preferences.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Preferences_Hover.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Preferences_Active.png ../releases/Linux/sIBL_GUI/resources/
+cp ../src/resources/Toolbar.png ../releases/Linux/sIBL_GUI/resources/
+cp -r ../src/templates/ ../releases/Linux/sIBL_GUI/
+cp -r ../src/components/ ../releases/Linux/sIBL_GUI/
+echo ----------------------------------------------------------------
+echo Release - End
+echo ----------------------------------------------------------------
+
+#! Templates Textile Files Cleanup.
+echo ----------------------------------------------------------------
+echo Templates Textile Files Cleanup - Begin
+echo ----------------------------------------------------------------
+#! XSI_MR_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/XSI/XSI_MR_Standard/help/XSI_MR_Standard Template Manual"
+
+#! Softimage_MR_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/Softimage/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual"
+
+#! Maya_MR_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/Maya/Maya_MR_Standard/help/Maya_MR_Standard Template Manual"
+
+#! Maya_RfM_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/Maya/Maya_RfM_Standard/help/Maya_RfM_Standard Template Manual"
+
+#! Maya_Turtle_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/Maya/Maya_Turtle_Standard/help/Maya_Turtle_Standard Template Manual"
+
+#! Maya_VRay_Dome_Light Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/Maya/Maya_VRay_Dome_Light/help/Maya_VRay_Dome_Light Template Manual"
+
+#! Maya_VRay_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/Maya/Maya_VRay_Standard/help/Maya_VRay_Standard Template Manual"
+
+#! 3dsMax_MR_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/3dsMax/3dsMax_MR_Standard/help/3dsMax_MR_Standard Template Manual"
+
+#! 3dsMax_Scanline_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/3dsMax/3dsMax_Scanline_Standard/help/3dsMax_Scanline_Standard Template Manual"
+
+#! 3dsMax_VRay_Standard Textile Template Documentation Removal.
+rm "../releases/Linux/sIBL_GUI/templates/3dsMax/3dsMax_VRay_Standard/help/3dsMax_VRay_Standard Template Manual"
+echo ----------------------------------------------------------------
+echo Templates Textile Files Cleanup - End
+echo ----------------------------------------------------------------
+
+#! sIBL_GUI Cleanup.
+echo ----------------------------------------------------------------
+echo Cleanup - Begin
+echo ----------------------------------------------------------------
+python ./sIBL_GUI_recursiveRemove.py ../releases/Linux/sIBL_GUI/ .pyc
+python ./sIBL_GUI_recursiveRemove.py ../releases/Linux/sIBL_GUI/ .pyo
+python ./sIBL_GUI_recursiveRemove.py ../releases/Linux/sIBL_GUI/ .DS_Store
+python ./sIBL_GUI_recursiveRemove.py ../releases/Linux/sIBL_GUI/ Thumbs.db
+echo ----------------------------------------------------------------
+echo Cleanup - End
+echo ----------------------------------------------------------------
