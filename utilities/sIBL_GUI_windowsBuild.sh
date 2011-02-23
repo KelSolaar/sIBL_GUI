1a01d0ad3f4e4a2b269e5c2e9075fc2bd27fe654
Thomas Mansencal <thomas.mansencal@gmail.com>
2011-02-23 14:40:12 +0100

    Fix errors in "sIBL_GUI_windowsBuild.sh" utility file.

--- utilities/sIBL_GUI_windowsBuild.sh
+++ utilities/sIBL_GUI_windowsBuild.sh
@@ -75,34 +75,34 @@ echo ----------------------------------------------------------------
 echo Templates Textile Files Cleanup - Begin
 echo ----------------------------------------------------------------
 #! XSI_MR_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/XSI_MR_Standard/help/XSI_MR_Standard Template Manual"
+rm "$DEPLOYMENT/templates/XSI_MR_Standard/help/XSI_MR_Standard Template Manual"
 
 #! Softimage_MR_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual"
+rm "$DEPLOYMENT/templates/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual"
 
 #! Maya_MR_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/Maya_MR_Standard/help/Maya_MR_Standard Template Manual"
+rm "$DEPLOYMENT/templates/Maya_MR_Standard/help/Maya_MR_Standard Template Manual"
 
 #! Maya_RfM_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/Maya_RfM_Standard/help/Maya_RfM_Standard Template Manual"
+rm "$DEPLOYMENT/templates/Maya_RfM_Standard/help/Maya_RfM_Standard Template Manual"
 
 #! Maya_Turtle_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/Maya_Turtle_Standard/help/Maya_Turtle_Standard Template Manual"
+rm "$DEPLOYMENT/templates/Maya_Turtle_Standard/help/Maya_Turtle_Standard Template Manual"
 
 #! Maya_VRay_Dome_Light Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/Maya_VRay_Dome_Light/help/Maya_VRay_Dome_Light Template Manual"
+rm "$DEPLOYMENT/templates/Maya_VRay_Dome_Light/help/Maya_VRay_Dome_Light Template Manual"
 
 #! Maya_VRay_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/Maya_VRay_Standard/help/Maya_VRay_Standard Template Manual"
+rm "$DEPLOYMENT/templates/Maya_VRay_Standard/help/Maya_VRay_Standard Template Manual"
 
 #! 3dsMax_MR_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/3dsMax_MR_Standard/help/3dsMax_MR_Standard Template Manual"
+rm "$DEPLOYMENT/templates/3dsMax_MR_Standard/help/3dsMax_MR_Standard Template Manual"
 
 #! 3dsMax_Scanline_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/3dsMax_Scanline_Standard/help/3dsMax_Scanline_Standard Template Manual"
+rm "$DEPLOYMENT/templates/3dsMax_Scanline_Standard/help/3dsMax_Scanline_Standard Template Manual"
 
 #! 3dsMax_VRay_Standard Textile Template Documentation Removal.
-del "%DEPLOYMENT/templates/3dsMax_VRay_Standard/help/3dsMax_VRay_Standard Template Manual"
+rm "$DEPLOYMENT/templates/3dsMax_VRay_Standard/help/3dsMax_VRay_Standard Template Manual"
 echo ----------------------------------------------------------------
 echo Templates Textile Files Cleanup - End
 echo ----------------------------------------------------------------
