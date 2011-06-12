adf990624857905524c822daf3bcfff3a71e9a00
KelSolaar <thomas.mansencal@gmail.com>
2011-06-12 14:14:06 +0100

    Add "databaseOperations" component.

---
+++ src/components/addons/databaseOperations/databaseOperations.py
@@ -0,0 +1,419 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#***********************************************************************************************
+#
+# Copyright (C) 2008 - 2011 - Thomas Mansencal - thomas.mansencal@gmail.com
+#
+#***********************************************************************************************
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#***********************************************************************************************
+#
+# The Following Code Is Protected By GNU GPL V3 Licence.
+#
+#***********************************************************************************************
+#
+# If You Are A HDRI Ressources Vendor And Are Interested In Making Your Sets SmartIBL Compliant:
+# Please Contact Us At HDRLabs:
+# Christian Bloch - blochi@edenfx.com
+# Thomas Mansencal - thomas.mansencal@gmail.com
+#
+#***********************************************************************************************
+
+"""
+************************************************************************************************
+***	databaseOperations.py
+***
+***	Platform:
+***		Windows, Linux, Mac Os X
+***
+***	Description:
+***		Database Operations Component Module.
+***
+***	Others:
+***
+************************************************************************************************
+"""
+
+#***********************************************************************************************
+#***	Python Begin
+#***********************************************************************************************
+
+#***********************************************************************************************
+#***	External Imports
+#***********************************************************************************************
+import logging
+import os
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+#***********************************************************************************************
+#***	Internal Imports
+#***********************************************************************************************
+import dbUtilities.common
+import foundations.core as core
+import foundations.exceptions
+import ui.widgets.messageBox as messageBox
+from globals.constants import Constants
+from manager.uiComponent import UiComponent
+
+#***********************************************************************************************
+#***	Global Variables
+#***********************************************************************************************
+LOGGER = logging.getLogger(Constants.logger)
+
+#***********************************************************************************************
+#***	Module Classes And Definitions
+#***********************************************************************************************
+class DatabaseOperations(UiComponent):
+	"""
+	This Class Is The DatabaseOperations Class.
+	"""
+
+	@core.executionTrace
+	def __init__(self, name=None, uiFile=None):
+		"""
+		This Method Initializes The Class.
+		
+		@param name: Component Name. ( String )
+		@param uiFile: Ui File. ( String )
+		"""
+
+		LOGGER.debug("> Initializing '{0}()' Class.".format(self.__class__.__name__))
+
+		UiComponent.__init__(self, name=name, uiFile=uiFile)
+
+		# --- Setting Class Attributes. ---
+		self.deactivatable = True
+
+		self._uiPath = "ui/Database_Operations.ui"
+
+		self._container = None
+
+		self._coreDb = None
+		self._corePreferencesManager = None
+		self._coreDatabaseBrowser = None
+		self._coreTemplatesOutliner = None
+
+	#***************************************************************************************
+	#***	Attributes Properties
+	#***************************************************************************************
+	@property
+	def uiPath(self):
+		"""
+		This Method Is The Property For The _uiPath Attribute.
+
+		@return: self._uiPath. ( String )
+		"""
+
+		return self._uiPath
+
+	@uiPath.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def uiPath(self, value):
+		"""
+		This Method Is The Setter Method For The _uiPath Attribute.
+
+		@param value: Attribute Value. ( String )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Read Only!".format("uiPath"))
+
+	@uiPath.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def uiPath(self):
+		"""
+		This Method Is The Deleter Method For The _uiPath Attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Not Deletable!".format("uiPath"))
+
+	@property
+	def container(self):
+		"""
+		This Method Is The Property For The _container Attribute.
+
+		@return: self._container. ( QObject )
+		"""
+
+		return self._container
+
+	@container.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def container(self, value):
+		"""
+		This Method Is The Setter Method For The _container Attribute.
+
+		@param value: Attribute Value. ( QObject )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Read Only!".format("container"))
+
+	@container.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def container(self):
+		"""
+		This Method Is The Deleter Method For The _container Attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Not Deletable!".format("container"))
+
+	@property
+	def coreDb(self):
+		"""
+		This Method Is The Property For The _coreDb Attribute.
+
+		@return: self._coreDb. ( Object )
+		"""
+
+		return self._coreDb
+
+	@coreDb.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def coreDb(self, value):
+		"""
+		This Method Is The Setter Method For The _coreDb Attribute.
+
+		@param value: Attribute Value. ( Object )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Read Only!".format("coreDb"))
+
+	@coreDb.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def coreDb(self):
+		"""
+		This Method Is The Deleter Method For The _coreDb Attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Not Deletable!".format("coreDb"))
+
+	@property
+	def corePreferencesManager(self):
+		"""
+		This Method Is The Property For The _corePreferencesManager Attribute.
+
+		@return: self._corePreferencesManager. ( Object )
+		"""
+
+		return self._corePreferencesManager
+
+	@corePreferencesManager.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def corePreferencesManager(self, value):
+		"""
+		This Method Is The Setter Method For The _corePreferencesManager Attribute.
+
+		@param value: Attribute Value. ( Object )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Read Only!".format("corePreferencesManager"))
+
+	@corePreferencesManager.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def corePreferencesManager(self):
+		"""
+		This Method Is The Deleter Method For The _corePreferencesManager Attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Not Deletable!".format("corePreferencesManager"))
+
+	@property
+	def coreDatabaseBrowser(self):
+		"""
+		This Method Is The Property For The _coreDatabaseBrowser Attribute.
+
+		@return: self._coreDatabaseBrowser. ( Object )
+		"""
+
+		return self._coreDatabaseBrowser
+
+	@coreDatabaseBrowser.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def coreDatabaseBrowser(self, value):
+		"""
+		This Method Is The Setter Method For The _coreDatabaseBrowser Attribute.
+
+		@param value: Attribute Value. ( Object )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Read Only!".format("coreDatabaseBrowser"))
+
+	@coreDatabaseBrowser.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def coreDatabaseBrowser(self):
+		"""
+		This Method Is The Deleter Method For The _coreDatabaseBrowser Attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Not Deletable!".format("coreDatabaseBrowser"))
+
+	@property
+	def coreTemplatesOutliner(self):
+		"""
+		This Method Is The Property For The _coreTemplatesOutliner Attribute.
+
+		@return: self._coreTemplatesOutliner. ( Object )
+		"""
+
+		return self._coreTemplatesOutliner
+
+	@coreTemplatesOutliner.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def coreTemplatesOutliner(self, value):
+		"""
+		This Method Is The Setter Method For The _coreTemplatesOutliner Attribute.
+
+		@param value: Attribute Value. ( Object )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Read Only!".format("coreTemplatesOutliner"))
+
+	@coreTemplatesOutliner.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def coreTemplatesOutliner(self):
+		"""
+		This Method Is The Deleter Method For The _coreTemplatesOutliner Attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' Attribute Is Not Deletable!".format("coreTemplatesOutliner"))
+
+
+	#***************************************************************************************
+	#***	Class Methods
+	#***************************************************************************************
+	@core.executionTrace
+	def activate(self, container):
+		"""
+		This Method Activates The Component.
+		
+		@param container: Container To Attach The Component To. ( QObject )
+		"""
+
+		LOGGER.debug("> Activating '{0}' Component.".format(self.__class__.__name__))
+
+		self.uiFile = os.path.join(os.path.dirname(core.getModule(self).__file__), self._uiPath)
+		self._container = container
+		self._settings = self._container.settings
+		self._settingsSection = self.name
+
+		self._coreDb = self._container.componentsManager.components["core.db"].interface
+		self._corePreferencesManager = self._container.componentsManager.components["core.preferencesManager"].interface
+		self._coreDatabaseBrowser = self._container.componentsManager.components["core.databaseBrowser"].interface
+		self._coreTemplatesOutliner = self._container.componentsManager.components["core.templatesOutliner"].interface
+
+		self._activate()
+
+	@core.executionTrace
+	def deactivate(self):
+		"""
+		This Method Deactivates The Component.
+		"""
+
+		LOGGER.debug("> Deactivating '{0}' Component.".format(self.__class__.__name__))
+
+		self.uiFile = None
+		self._container = None
+		self._settings = None
+		self._settingsSection = None
+
+		self._corePreferencesManager = None
+		self._coreDatabaseBrowser = None
+		self._coreTemplatesOutliner = None
+
+		self._deactivate()
+
+	@core.executionTrace
+	def initializeUi(self):
+		"""
+		This Method Initializes The Component Ui.
+		"""
+
+		LOGGER.debug("> Initializing '{0}' Component Ui.".format(self.__class__.__name__))
+
+		# Signals / Slots.
+		if not self._container.parameters.databaseReadOnly:
+			self.ui.Synchronize_Database_pushButton.clicked.connect(self.Synchronize_Database_pushButton_OnClicked)
+		else:
+			LOGGER.info("{0} | Database Operations Capabilities Deactivated By '{1}' Command Line Parameter Value!".format(self.__class__.__name__, "databaseReadOnly"))
+
+	@core.executionTrace
+	def uninitializeUi(self):
+		"""
+		This Method Uninitializes The Component Ui.
+		"""
+
+		LOGGER.debug("> Uninitializing '{0}' Component Ui.".format(self.__class__.__name__))
+
+		# Signals / Slots.
+		not self._container.parameters.databaseReadOnly and	self.ui.Synchronize_Database_pushButton.clicked.disconnect(self.Synchronize_Database_pushButton_OnClicked)
+
+	@core.executionTrace
+	def addWidget(self):
+		"""
+		This Method Adds The Component Widget To The Container.
+		"""
+
+		LOGGER.debug("> Adding '{0}' Component Widget.".format(self.__class__.__name__))
+
+		self._corePreferencesManager.ui.Others_Preferences_gridLayout.addWidget(self.ui.Database_Operations_groupBox)
+
+	@core.executionTrace
+	def removeWidget(self):
+		"""
+		This Method Removes The Component Widget From The Container.
+		"""
+
+		LOGGER.debug("> Removing '{0}' Component Widget.".format(self.__class__.__name__))
+
+		self._corePreferencesManager.ui.findChild(QGridLayout, "Others_Preferences_gridLayout").removeWidget(self.ui)
+		self.ui.Database_Operations_groupBox.setParent(None)
+
+	@core.executionTrace
+	def Synchronize_Database_pushButton_OnClicked(self, checked):
+		"""
+		This Method Is Triggered When Synchronize_Database_pushButton Is Clicked.
+		
+		@param checked: Checked State. ( Boolean )
+		"""
+		
+		self.synchronizeDatabase()
+
+	@core.executionTrace
+	def synchronizeDatabase(self):
+		"""
+		This Method Synchronizes The Database.
+		"""
+
+		dbTypes = ({"type" : "Ibl Set", "getMethod" : dbUtilities.common.getIblSets, "updateContentMethod" : dbUtilities.common.updateIblSetContent, "updateLocationMethod" : self._coreDatabaseBrowser.updateIblSetLocation, "refreshModelMethod" : self._coreDatabaseBrowser.Database_Browser_listView_extendedRefreshModel},
+					{"type" : "Template", "getMethod" : dbUtilities.common.getTemplates, "updateContentMethod" : dbUtilities.common.updateTemplateContent, "updateLocationMethod" : self._coreTemplatesOutliner.updateTemplateLocation, "refreshModelMethod" : self._coreTemplatesOutliner.Templates_Outliner_treeView_refreshModel})
+		
+		for dbType in dbTypes:
+			needModelRefresh = False
+			for item in dbType["getMethod"](self._coreDb.dbSession):
+				if item.path:
+					if os.path.exists(item.path):
+						if dbType["updateContentMethod"](self._coreDb.dbSession, item):
+							LOGGER.info("{0} | '{1}' {2} Has Been Synchronized!".format(self.__class__.__name__, item.name, dbType["type"]))
+							needModelRefresh = True
+					else:
+						if messageBox.messageBox("Question", "Error", "{0} | '{1}' {2} File Is Missing, Would You Like To Update It's Location?".format(self.__class__.__name__, item.name, dbType["type"]), QMessageBox.Critical, QMessageBox.Yes | QMessageBox.No) == 16384:
+								dbType["updateLocationMethod"](item)
+			needModelRefresh and dbType["refreshModelMethod"]()
+
+#***********************************************************************************************
+#***	Python End
+#***********************************************************************************************
