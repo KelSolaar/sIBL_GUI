8fe96e2ecbb20bdafecfb7e15f22dbb49ae92ccf
KelSolaar <thomas.mansencal@gmail.com>
2011-07-12 00:30:04 +0200

    Update pyinstaller "hook-foundations" file.

--- utilities/pyinstaller/hook-foundations.py
+++ utilities/pyinstaller/hook-foundations.py
@@ -1,1 +1,15 @@
-hiddenimports = ["foundations.pkzip", "foundations.strings", "libraries.freeImage.freeImage", "migrate.exceptions", "migrate.versioning.api", "ui.widgets.search_QLineEdit", "ui.widgets.variable_QPushButton", "PyQt4.QtNetwork", "PyQt4.QtWebKit", "sqlalchemy.ext", "sqlalchemy.ext.declarative", "sqlalchemy", "sqlalchemy.orm", "win32com.client"]
+hiddenimports = ["foundations.pkzip",
+				"foundations.rotatingBackup",
+				"foundations.strings",
+				"libraries.freeImage.freeImage",
+				"migrate.exceptions",
+				"migrate.versioning.api",
+				"ui.widgets.search_QLineEdit",
+				"ui.widgets.variable_QPushButton",
+				"PyQt4.QtNetwork",
+				"PyQt4.QtWebKit",
+				"sqlalchemy.ext",
+				"sqlalchemy.ext.declarative",
+				"sqlalchemy",
+				"sqlalchemy.orm",
+				"win32com.client"]
