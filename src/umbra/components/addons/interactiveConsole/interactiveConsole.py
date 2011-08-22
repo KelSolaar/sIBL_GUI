40960a23edf5f1086fe76c8dca954d57c8dd3a01
KelSolaar <thomas.mansencal@gmail.com>
2011-08-22 23:51:03 +0100

    Implement "interactiveConsole" component skeleton.

---
+++ src/umbra/components/addons/interactiveConsole/interactiveConsole.py
@@ -0,0 +1,382 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**interactiveConsole.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+	This module defines the :class:`InteractiveConsole` Component Interface class.
+
+**Others:**
+
+"""
+
+#***********************************************************************************************
+#***	External imports.
+#***********************************************************************************************
+import code
+import inspect
+import logging
+import os
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+#***********************************************************************************************
+#***	Internal imports.
+#***********************************************************************************************
+import foundations.core as core
+import foundations.exceptions
+from manager.uiComponent import UiComponent
+from umbra.globals.constants import Constants
+from umbra.globals.runtimeConstants import RuntimeConstants
+from umbra.globals.uiConstants import UiConstants
+
+#***********************************************************************************************
+#***	Module attributes.
+#***********************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2011 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+LOGGER = logging.getLogger(Constants.logger)
+
+#***********************************************************************************************
+#***	Module classes and definitions.
+#***********************************************************************************************
+class InteractiveConsole(UiComponent):
+	"""
+	| This class is the :mod:`umbra.components.addons.interactiveConsole.interactiveConsole` Component Interface class.
+	"""
+
+	@core.executionTrace
+	def __init__(self, name=None, uiFile=None):
+		"""
+		This method initializes the class.
+
+		:param name: Component name. ( String )
+		:param uiFile: Ui file. ( String )
+		"""
+
+		LOGGER.debug("> Initializing '{0}()' class.".format(self.__class__.__name__))
+
+		UiComponent.__init__(self, name=name, uiFile=uiFile)
+
+		# --- Setting class attributes. ---
+		self.deactivatable = True
+
+		self.__uiPath = "ui/Interactive_Console.ui"
+		self.__dockArea = 8
+
+		self.__container = None
+
+		self.__locals = None
+		self.__memoryHandlerStackDepth = None
+
+	#***********************************************************************************************
+	#***	Attributes properties.
+	#***********************************************************************************************
+	@property
+	def uiPath(self):
+		"""
+		This method is the property for **self.__uiPath** attribute.
+
+		:return: self.__uiPath. ( String )
+		"""
+
+		return self.__uiPath
+
+	@uiPath.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def uiPath(self, value):
+		"""
+		This method is the setter method for **self.__uiPath** attribute.
+
+		:param value: Attribute value. ( String )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is read only!".format("uiPath"))
+
+	@uiPath.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def uiPath(self):
+		"""
+		This method is the deleter method for **self.__uiPath** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("uiPath"))
+
+	@property
+	def dockArea(self):
+		"""
+		This method is the property for **self.__dockArea** attribute.
+
+		:return: self.__dockArea. ( Integer )
+		"""
+
+		return self.__dockArea
+
+	@dockArea.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def dockArea(self, value):
+		"""
+		This method is the setter method for **self.__dockArea** attribute.
+
+		:param value: Attribute value. ( Integer )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is read only!".format("dockArea"))
+
+	@dockArea.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def dockArea(self):
+		"""
+		This method is the deleter method for **self.__dockArea** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("dockArea"))
+
+	@property
+	def container(self):
+		"""
+		This method is the property for **self.__container** attribute.
+
+		:return: self.__container. ( QObject )
+		"""
+
+		return self.__container
+
+	@container.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def container(self, value):
+		"""
+		This method is the setter method for **self.__container** attribute.
+
+		:param value: Attribute value. ( QObject )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is read only!".format("container"))
+
+	@container.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def container(self):
+		"""
+		This method is the deleter method for **self.__container** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("container"))
+
+	@property
+	def locals(self):
+		"""
+		This method is the property for **self.__locals** attribute.
+
+		:return: self.__locals. ( Dictionary )
+		"""
+
+		return self.__locals
+
+	@locals.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def locals(self, value):
+		"""
+		This method is the setter method for **self.__locals** attribute.
+
+		:param value: Attribute value. ( Dictionary )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is read only!".format("locals"))
+
+	@locals.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def locals(self):
+		"""
+		This method is the deleter method for **self.__locals** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("locals"))
+
+	@property
+	def memoryHandlerStackDepth(self):
+		"""
+		This method is the property for **self.__memoryHandlerStackDepth** attribute.
+
+		:return: self.__memoryHandlerStackDepth. ( Integer )
+		"""
+
+		return self.__memoryHandlerStackDepth
+
+	@memoryHandlerStackDepth.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def memoryHandlerStackDepth(self, value):
+		"""
+		This method is the setter method for **self.__memoryHandlerStackDepth** attribute.
+
+		:param value: Attribute value. ( Integer )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is read only!".format("memoryHandlerStackDepth"))
+
+	@memoryHandlerStackDepth.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def memoryHandlerStackDepth(self):
+		"""
+		This method is the deleter method for **self.__memoryHandlerStackDepth** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("memoryHandlerStackDepth"))
+
+	#***********************************************************************************************
+	#***	Class methods.
+	#***********************************************************************************************
+	@core.executionTrace
+	def activate(self, container):
+		"""
+		This method activates the Component.
+
+		:param container: Container to attach the Component to. ( QObject )
+		:return: Method success. ( Boolean )
+		"""
+
+		LOGGER.debug("> Activating '{0}' Component.".format(self.__class__.__name__))
+
+		self.uiFile = os.path.join(os.path.dirname(core.getModule(self).__file__), self.__uiPath)
+		self.__container = container
+
+		self.__getsLocals()
+		self.__console = code.InteractiveConsole(self.__locals)
+
+		return UiComponent.activate(self)
+
+	@core.executionTrace
+	def deactivate(self):
+		"""
+		This method deactivates the Component.
+
+		:return: Method success. ( Boolean )
+		"""
+
+		LOGGER.debug("> Deactivating '{0}' Component.".format(self.__class__.__name__))
+
+		self.uiFile = None
+		self.__container = None
+
+		self.__console = None
+
+		return UiComponent.deactivate(self)
+
+	@core.executionTrace
+	def initializeUi(self):
+		"""
+		This method initializes the Component ui.
+		
+		:return: Method success. ( Boolean )		
+		"""
+
+		LOGGER.debug("> Initializing '{0}' Component ui.".format(self.__class__.__name__))
+
+		# Signals / Slots.
+		self.__container.timer.timeout.connect(self.__Interactive_Console_Output_textEdit_refreshUi)
+		self.ui.Evaluate_Input_pushButton.clicked.connect(self.__Evaluate_Input_pushButton__clicked)
+
+		return True
+
+	@core.executionTrace
+	def uninitializeUi(self):
+		"""
+		This method uninitializes the Component ui.
+		
+		:return: Method success. ( Boolean )		
+		"""
+
+		LOGGER.debug("> Uninitializing '{0}' Component ui.".format(self.__class__.__name__))
+
+		# Signals / Slots.
+		self.__container.timer.timeout.disconnect(self.__Interactive_Console_Output_textEdit_refreshUi)
+		self.ui.Evaluate_Input_pushButton.clicked.disconnect(self.__Evaluate_Input_pushButton__clicked)
+
+		return True
+
+	@core.executionTrace
+	def addWidget(self):
+		"""
+		This method adds the Component Widget to the container.
+
+		:return: Method success. ( Boolean )		
+		"""
+
+		LOGGER.debug("> Adding '{0}' Component Widget.".format(self.__class__.__name__))
+
+		self.__container.addDockWidget(Qt.DockWidgetArea(self.__dockArea), self.ui)
+
+		return True
+
+	@core.executionTrace
+	def removeWidget(self):
+		"""
+		This method removes the Component Widget from the container.
+
+		:return: Method success. ( Boolean )		
+		"""
+
+		LOGGER.debug("> Removing '{0}' Component Widget.".format(self.__class__.__name__))
+
+		self.__container.removeDockWidget(self.ui)
+		self.ui.setParent(None)
+
+		return True
+
+	# @core.executionTrace
+	def __Interactive_Console_Output_textEdit_setUi(self):
+		"""
+		This method sets the **Logging_TextEdit** Widget.
+		"""
+
+		self.ui.Interactive_Console_Output_textEdit.setPlainText(QString("".join(self.__container.loggingSessionHandlerStream.stream)))
+		self.ui.Interactive_Console_Output_textEdit.moveCursor(QTextCursor.End)
+		self.ui.Interactive_Console_Output_textEdit.ensureCursorVisible()
+
+	# @core.executionTrace
+	def __Interactive_Console_Output_textEdit_refreshUi(self):
+		"""
+		This method updates the **Interactive_Console_Output_textEdit** Widget.
+		"""
+
+		memoryHandlerStackDepth = len(self.__container.loggingSessionHandlerStream.stream)
+		if memoryHandlerStackDepth != self.__memoryHandlerStackDepth:
+			self.__Interactive_Console_Output_textEdit_setUi()
+			self.__memoryHandlerStackDepth = memoryHandlerStackDepth
+
+	@core.executionTrace
+	def __Evaluate_Input_pushButton__clicked(self, checked):
+		"""
+		This method is triggered when **Evaluate_Input_pushButton** is clicked.
+
+		:param checked: Checked state. ( Boolean )
+		"""
+
+		self.__console.runcode(str(self.ui.Interactive_Console_Input_textEdit.toPlainText()))
+
+		self.__Interactive_Console_Output_textEdit_refreshUi()
+
+	def __getsLocals(self):
+		"""
+		This method gets the locals for the interactive console.
+
+		:return: Method success. ( Boolean )
+		"""
+
+		self.__locals = {}
+
+		for globals in (Constants, RuntimeConstants, UiConstants):
+			self.__locals[globals.__name__] = globals
+
+		self.__locals[Constants.applicationName] = self.__container
+		self.__locals["componentsManager"] = self.__container.componentsManager
+
+		return True
