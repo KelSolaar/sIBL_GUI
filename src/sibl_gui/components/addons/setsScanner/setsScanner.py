ac489d51af2a034ad08e1b805c8759d425a915a1
Thomas Mansencal <thomas.mansencal@gmail.com>
2011-11-07 11:18:30 +0000

    Refactor "setsScanner" component.

--- src/sibl_gui/components/addons/setsScanner/setsScanner.py
+++ src/sibl_gui/components/addons/setsScanner/setsScanner.py
@@ -19,7 +19,6 @@
 #***********************************************************************************************
 import logging
 import os
-import re
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -30,11 +29,10 @@ import foundations.core as core
 import foundations.exceptions
 import foundations.namespace
 import sibl_gui.components.core.db.utilities.common as dbCommon
-import sibl_gui.components.core.db.utilities.types as dbTypes
 import umbra.engine
 import umbra.ui.widgets.messageBox as messageBox
-from foundations.walkers import OsWalker
 from manager.qobjectComponent import QObjectComponent
+from sibl_gui.components.addons.setsScanner.workers import SetsScanner_Worker
 from umbra.globals.constants import Constants
 
 #***********************************************************************************************
@@ -47,208 +45,13 @@ __maintainer__ = "Thomas Mansencal"
 __email__ = "thomas.mansencal@gmail.com"
 __status__ = "Production"
 
-__all__ = ["LOGGER", "SetsScanner_Worker", "SetsScanner"]
+__all__ = ["LOGGER", "SetsScanner"]
 
 LOGGER = logging.getLogger(Constants.logger)
 
 #***********************************************************************************************
 #***	Module classes and definitions.
 #***********************************************************************************************
-class SetsScanner_Worker(QThread):
-	"""
-	This class is a `QThread <http://doc.qt.nokia.com/4.7/qthread.html>`_ subclass used to retrieve new Ibl Sets from Database registered directories parents.
-	"""
-
-	# Custom signals definitions.
-	iblSetsRetrieved = pyqtSignal(dict)
-
-	@core.executionTrace
-	def __init__(self, parent):
-		"""
-		This method initializes the class.
-
-		:param parent: Object parent. ( QObject )
-		"""
-
-		LOGGER.debug("> Initializing '{0}()' class.".format(self.__class__.__name__))
-
-		QThread.__init__(self, parent)
-
-		# --- Setting class attributes. ---
-		self.__container = parent
-
-		self.__dbSession = self.__container.coreDb.dbSessionMaker()
-
-		self.__newIblSets = None
-
-		self.__extension = "ibl"
-
-	#***********************************************************************************************
-	#***	Attributes properties.
-	#***********************************************************************************************
-	@property
-	def container(self):
-		"""
-		This method is the property for **self.__container** attribute.
-
-		:return: self.__container. ( QObject )
-		"""
-
-		return self.__container
-
-	@container.setter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def container(self, value):
-		"""
-		This method is the setter method for **self.__container** attribute.
-
-		:param value: Attribute value. ( QObject )
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "container"))
-
-	@container.deleter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def container(self):
-		"""
-		This method is the deleter method for **self.__container** attribute.
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "container"))
-
-	@property
-	def dbSession(self):
-		"""
-		This method is the property for **self.__dbSession** attribute.
-
-		:return: self.__dbSession. ( Object )
-		"""
-
-		return self.__dbSession
-
-	@dbSession.setter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def dbSession(self, value):
-		"""
-		This method is the setter method for **self.__dbSession** attribute.
-
-		:param value: Attribute value. ( Object )
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "dbSession"))
-
-	@dbSession.deleter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def dbSession(self):
-		"""
-		This method is the deleter method for **self.__dbSession** attribute.
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "dbSession"))
-
-	@property
-	def extension(self):
-		"""
-		This method is the property for **self.__extension** attribute.
-
-		:return: self.__extension. ( String )
-		"""
-
-		return self.__extension
-
-	@extension.setter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def extension(self, value):
-		"""
-		This method is the setter method for **self.__extension** attribute.
-
-		:param value: Attribute value. ( String )
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "extension"))
-
-	@extension.deleter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def extension(self):
-		"""
-		This method is the deleter method for **self.__extension** attribute.
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "extension"))
-
-	@property
-	def newIblSets(self):
-		"""
-		This method is the property for **self.__newIblSets** attribute.
-
-		:return: self.__newIblSets. ( Dictionary )
-		"""
-
-		return self.__newIblSets
-
-	@newIblSets.setter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def newIblSets(self, value):
-		"""
-		This method is the setter method for **self.__newIblSets** attribute.
-
-		:param value: Attribute value. ( Dictionary )
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "newIblSets"))
-
-	@newIblSets.deleter
-	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
-	def newIblSets(self):
-		"""
-		This method is the deleter method for **self.__newIblSets** attribute.
-		"""
-
-		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "newIblSets"))
-
-	#***********************************************************************************************
-	#***	Class methods.
-	#***********************************************************************************************
-	@core.executionTrace
-	def run(self):
-		"""
-		This method starts the QThread.
-		"""
-
-		self.scanIblSetsDirectories()
-
-	@core.executionTrace
-	@foundations.exceptions.exceptionsHandler(None, False, Exception)
-	def scanIblSetsDirectories(self):
-		"""
-		This method scans Ibl Sets directories.
-
-		:return: Method success. ( Boolean )
-		"""
-
-		LOGGER.info("{0} | Scanning Ibl Sets directories for new Ibl Sets!".format(self.__class__.__name__))
-
-		self.__newIblSets = {}
-		paths = [path[0] for path in self.__dbSession.query(dbTypes.DbIblSet.path).all()]
-		directories = set((os.path.normpath(os.path.join(os.path.dirname(path), "..")) for path in paths))
-		needModelRefresh = False
-		for directory in directories:
-			if os.path.exists(directory):
-				osWalker = OsWalker(directory)
-				osWalker.walk(("\.{0}$".format(self.__extension),), ("\._",))
-				for iblSet, path in osWalker.files.items():
-					if not dbCommon.filterIblSets(self.__dbSession, "^{0}$".format(re.escape(path)), "path"):
-						needModelRefresh = True
-						self.__newIblSets[iblSet] = path
-			else:
-				LOGGER.warning("!> '{0}' directory doesn't exists and won't be scanned for new Ibl Sets!".format(directory))
-
-		self.__dbSession.close()
-
-		LOGGER.info("{0} | Scanning done!".format(self.__class__.__name__))
-		needModelRefresh and self.iblSetsRetrieved.emit(self.__newIblSets)
-		return True
-
 class SetsScanner(QObjectComponent):
 	"""
 	| This class is the :mod:`umbra.components.addons.setsScanner.setsScanner` Component Interface class.
---
+++ src/sibl_gui/components/addons/setsScanner/workers.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**models.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+	This module defines the :class:`sibl_gui.components.addons.setsScanner.setsScanner.SetsScanner` Component Interface class Workers.
+
+**Others:**
+
+"""
+
+#***********************************************************************************************
+#***	External imports.
+#***********************************************************************************************
+import logging
+import os
+import re
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+#***********************************************************************************************
+#***	Internal imports.
+#***********************************************************************************************
+import foundations.core as core
+import foundations.exceptions
+import sibl_gui.components.core.db.utilities.common as dbCommon
+import sibl_gui.components.core.db.utilities.types as dbTypes
+from foundations.walkers import OsWalker
+from umbra.globals.constants import Constants
+
+#***********************************************************************************************
+#***	Module attributes.
+#***********************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2011 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+__all__ = ["LOGGER", "SetsScanner_Worker"]
+
+LOGGER = logging.getLogger(Constants.logger)
+
+#***********************************************************************************************
+#***	Module classes and definitions.
+#***********************************************************************************************
+class SetsScanner_Worker(QThread):
+	"""
+	This class is a `QThread <http://doc.qt.nokia.com/4.7/qthread.html>`_ subclass used to retrieve new Ibl Sets from Database registered directories parents.
+	"""
+
+	# Custom signals definitions.
+	iblSetsRetrieved = pyqtSignal(dict)
+
+	@core.executionTrace
+	def __init__(self, parent):
+		"""
+		This method initializes the class.
+
+		:param parent: Object parent. ( QObject )
+		"""
+
+		LOGGER.debug("> Initializing '{0}()' class.".format(self.__class__.__name__))
+
+		QThread.__init__(self, parent)
+
+		# --- Setting class attributes. ---
+		self.__container = parent
+
+		self.__dbSession = self.__container.coreDb.dbSessionMaker()
+
+		self.__newIblSets = None
+
+		self.__extension = "ibl"
+
+	#***********************************************************************************************
+	#***	Attributes properties.
+	#***********************************************************************************************
+	@property
+	def container(self):
+		"""
+		This method is the property for **self.__container** attribute.
+
+		:return: self.__container. ( QObject )
+		"""
+
+		return self.__container
+
+	@container.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def container(self, value):
+		"""
+		This method is the setter method for **self.__container** attribute.
+
+		:param value: Attribute value. ( QObject )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "container"))
+
+	@container.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def container(self):
+		"""
+		This method is the deleter method for **self.__container** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "container"))
+
+	@property
+	def dbSession(self):
+		"""
+		This method is the property for **self.__dbSession** attribute.
+
+		:return: self.__dbSession. ( Object )
+		"""
+
+		return self.__dbSession
+
+	@dbSession.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def dbSession(self, value):
+		"""
+		This method is the setter method for **self.__dbSession** attribute.
+
+		:param value: Attribute value. ( Object )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "dbSession"))
+
+	@dbSession.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def dbSession(self):
+		"""
+		This method is the deleter method for **self.__dbSession** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "dbSession"))
+
+	@property
+	def extension(self):
+		"""
+		This method is the property for **self.__extension** attribute.
+
+		:return: self.__extension. ( String )
+		"""
+
+		return self.__extension
+
+	@extension.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def extension(self, value):
+		"""
+		This method is the setter method for **self.__extension** attribute.
+
+		:param value: Attribute value. ( String )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "extension"))
+
+	@extension.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def extension(self):
+		"""
+		This method is the deleter method for **self.__extension** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "extension"))
+
+	@property
+	def newIblSets(self):
+		"""
+		This method is the property for **self.__newIblSets** attribute.
+
+		:return: self.__newIblSets. ( Dictionary )
+		"""
+
+		return self.__newIblSets
+
+	@newIblSets.setter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def newIblSets(self, value):
+		"""
+		This method is the setter method for **self.__newIblSets** attribute.
+
+		:param value: Attribute value. ( Dictionary )
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is read only!".format(self.__class__.__name__, "newIblSets"))
+
+	@newIblSets.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def newIblSets(self):
+		"""
+		This method is the deleter method for **self.__newIblSets** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("{0} | '{1}' attribute is not deletable!".format(self.__class__.__name__, "newIblSets"))
+
+	#***********************************************************************************************
+	#***	Class methods.
+	#***********************************************************************************************
+	@core.executionTrace
+	def run(self):
+		"""
+		This method starts the QThread.
+		"""
+
+		self.scanIblSetsDirectories()
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def scanIblSetsDirectories(self):
+		"""
+		This method scans Ibl Sets directories.
+
+		:return: Method success. ( Boolean )
+		"""
+
+		LOGGER.info("{0} | Scanning Ibl Sets directories for new Ibl Sets!".format(self.__class__.__name__))
+
+		self.__newIblSets = {}
+		paths = [path[0] for path in self.__dbSession.query(dbTypes.DbIblSet.path).all()]
+		directories = set((os.path.normpath(os.path.join(os.path.dirname(path), "..")) for path in paths))
+		needModelRefresh = False
+		for directory in directories:
+			if os.path.exists(directory):
+				osWalker = OsWalker(directory)
+				osWalker.walk(("\.{0}$".format(self.__extension),), ("\._",))
+				for iblSet, path in osWalker.files.items():
+					if not dbCommon.filterIblSets(self.__dbSession, "^{0}$".format(re.escape(path)), "path"):
+						needModelRefresh = True
+						self.__newIblSets[iblSet] = path
+			else:
+				LOGGER.warning("!> '{0}' directory doesn't exists and won't be scanned for new Ibl Sets!".format(directory))
+
+		self.__dbSession.close()
+
+		LOGGER.info("{0} | Scanning done!".format(self.__class__.__name__))
+		needModelRefresh and self.iblSetsRetrieved.emit(self.__newIblSets)
+		return True
