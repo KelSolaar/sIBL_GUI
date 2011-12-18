b935daaa35574224ff758dfcf80e76ff59d09c27
KelSolaar <thomas.mansencal@gmail.com>
2011-12-18 01:15:09 +0100

    Code formatting.

--- utilities/darwinSetup.py
+++ utilities/darwinSetup.py
@@ -1,13 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**darwinSetup.py**
+
+**Platform:**
+	Mac Os X.
+
+**Description:**
+	This module defines the py2app configuration file.
+
+**Others:**
+
+"""
+
+#**********************************************************************************************************************
+#***	External imports.
+#**********************************************************************************************************************
 from setuptools import setup
 
-MAJOR_VERSION = 4
+#**********************************************************************************************************************
+#***	Module attributes.
+#**********************************************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2011 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+__all__ = ["MAJOR_VERSION", "APP", "DATA_FILES", "OPTIONS"]
+
+APPLICATION_NAME = "sIBL_GUI"
+MAJOR_VERSION = "4"
 
 APP = ["../../src/sIBL_GUI.py"]
 DATA_FILES = []
 OPTIONS = {"argv_emulation": True, "iconfile": "../../src/sibl_gui/resources/images/Icon_Light_256.icns"}
 
-setup(name="sIBL_GUI {0}".format(MAJOR_VERSION),
+#**********************************************************************************************************************
+#***	Module classes and definitions.
+#**********************************************************************************************************************
+setup(name="{0} {1}".format(APPLICATION_NAME, MAJOR_VERSION),
 	app=APP,
 	data_files=DATA_FILES,
 	options={"py2app": OPTIONS},
-	setup_requires=["py2app"],)
+	setup_requires=["py2app"])
