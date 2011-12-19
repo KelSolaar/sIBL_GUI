63ae1a3743bbb43027d4de501ec045ae950bf264
KelSolaar <thomas.mansencal@gmail.com>
2011-12-19 23:40:57 +0000

    Update "gatherFiles.sh" utility file.

--- utilities/gatherFiles.sh
+++ utilities/gatherFiles.sh
@@ -3,25 +3,36 @@ echo ----------------------------------------------------------------
 echo sIBL_GUI - Files Gathering
 echo ----------------------------------------------------------------
 
+export PROJECT=/Users/KelSolaar/Documents/Developement/sIBL_GUI
+
+export SUPPORT=$PROJECT/support/
+export RELEASES=$PROJECT/releases/
+export REPOSITORY=RELEASES/repository/
+export UTILITIES=$PROJECT/utilities
+
 #! Gathering folder cleanup.
-rm -rf ./releases/repository/*
-mkdir ./releases/repository/sIBL_GUI
+rm -rf $REPOSITORY/*
+mkdir $REPOSITORY/sIBL_GUI
 
 #! Windows gathering.
-cp ./utilities/nsis/sIBL_GUI_Setup.exe ./releases/repository/sIBL_GUI/sIBL_GUI.exe
+cp $UTILITIES/nsis/sIBL_GUI_Setup.exe $REPOSITORY/sIBL_GUI/sIBL_GUI.exe
 
 #! Mac Os X gathering.
-cp ./releases/Darwin/sIBL_GUI.dmg ./releases/repository/sIBL_GUI/sIBL_GUI.dmg
+cp $RELEASES/Darwin/sIBL_GUI.dmg $REPOSITORY/sIBL_GUI/sIBL_GUI.dmg
 
 #! sIBL_GUI Change Log gathering.
-cp -rf ./releases/Change_Log.html ./releases/repository/sIBL_GUI/
+cp -rf $RELEASES/Change_Log.html $REPOSITORY/sIBL_GUI/
 
 #! sIBL_GUI Releases file.
-cp -rf ./releases/sIBL_GUI_Releases.rc ./releases/repository/sIBL_GUI/
+cp -rf $RELEASES/sIBL_GUI_Releases.rc $REPOSITORY/sIBL_GUI/
 
-#! sIBL_GUI Manual / Help file.
-cp -rf ./support/documentation/help ./releases/repository/sIBL_GUI/Help
-rm ./releases/repository/sIBL_GUI/help/sIBL_GUI_Manual.rst
+#! sIBL_GUI Manual / Help files.
+cp -rf $SUPPORT/documentation/help $REPOSITORY/sIBL_GUI/Help
+rm $REPOSITORY/sIBL_GUI/help/sIBL_GUI_Manual.rst
 
-#! sIBL_GUI Api file.
-cp -rf ./support/documentation/sphinx/build/html ./releases/repository/sIBL_GUI/Api
+#! sIBL_GUI Api files.
+cp -rf $SUPPORT/documentation/sphinx/build/html $REPOSITORY/sIBL_GUI/Api
+
+#! sIBL_GUI Donations files.
+cp -rf $SUPPORT/donations $REPOSITORY/sIBL_GUI/Donations
+rm $REPOSITORY/sIBL_GUI/Donations/Make_A_Donation.rst
