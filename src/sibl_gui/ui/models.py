10892f3cc234abfa0b3f2e13b3941a8637f5c276
Thomas Mansencal <thomas.mansencal@gmail.com>
2012-08-19 10:41:33 +0100

    Update "sibl_gui.ui.models.GraphModel.data" method.

--- src/sibl_gui/ui/models.py
+++ src/sibl_gui/ui/models.py
@@ -91,16 +91,16 @@ class GraphModel(umbra.ui.models.GraphModel):
 			if hasattr(node, "roles"):
 				value = node.roles.get(role)
 				if role == Qt.DecorationRole:
-					return value is not None and sibl_gui.ui.common.getIcon(value) or QVariant()
+					return sibl_gui.ui.common.getIcon(value) if value is not None else QVariant()
 				else:
-					return value is not None and value or QVariant()
+					return value if value is not None else QVariant()
 		else:
 			attribute = self.getAttribute(node, index.column())
 			if attribute:
 				if hasattr(attribute, "roles"):
 					value = attribute.roles.get(role)
 					if role == Qt.DecorationRole:
-						return value is not None and sibl_gui.ui.common.getIcon(value) or QVariant()
+						return sibl_gui.ui.common.getIcon(value) if value is not None else QVariant()
 					else:
-						return value is not None and value or QVariant()
+						return value if value is not None else QVariant()
 		return QVariant()
